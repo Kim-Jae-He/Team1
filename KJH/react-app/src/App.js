e842acbe401f610a446159f517922bf5fb96d97b
Kim-Jae-He <jgim78628@gmail.com>
2024-03-26 19:44:52 +0900

    03-26

--- KJH/react-app/src/App.js
+++ KJH/react-app/src/App.js
@@ -1,13 +1,26 @@
 import styles from "./App.module.css"
 
+import Button from "./components/Button"
+
+import Header from "./Header"
+
 function App() {
   return (
     <>
       <div className={styles.nav}>Nav </div>
-      <div className={styles.header}>Header</div>
+      <Header />
       <div className={styles.listContainer}>ListContainer</div>
-      <div className={styles.footer}>footer</div>
+      <Button
+        style={{
+          fontSize: "14px",
+          backgroundColor: "green",
+          color: "white",
+        }}
+      >
+        New Issue
+      </Button>
 
+      <div className={styles.footer}>footer</div>
     </>
   )
 }
