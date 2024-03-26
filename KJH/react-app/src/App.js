96b4c839132badd87900460f8357dda9113ac6cd
Kim-Jae-He <jgim78628@gmail.com>
2024-03-26 15:28:06 +0900

    03-26

--- KJH/react-app/src/App.js
+++ KJH/react-app/src/App.js
@@ -1,36 +1,14 @@
-import "./App.css"
-import React,{useEffect, useState} from 'react';
-
-// useEffect
-/**
- * 
- * mount
- * unmount
- * update
- * 
- */
-
-
-// 1. 최상위에서 호출되야한다.
-// 2. 오직 react에서만 hook을 호출해야한다.
+import styles from "./App.module.css"
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className={styles.nav}>Nav </div>
+      <div className={styles.header}>Header</div>
+      <div className={styles.listContainer}>ListContainer</div>
+      <div className={styles.footer}>footer</div>
 
-  useEffect(() => console.log('hello'));
-
-  useEffect(()=> {
-    if(count===5){
-      console.log("this is five")
-    }
-  },[count]);
-
-  return (<div className="App">
-    <span>{count}</span>
-    <button onClick={() => setCount(count +1)}>+</button>
-
-
-  </div>
+    </>
   )
 }
 
